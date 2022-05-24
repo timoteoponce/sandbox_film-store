930a8815ef1efcda97ab5b09e97c2109d3e87700
Timoteo Ponce <ponce.timoteo@gmail.com>
2022-05-24 09:39:49 -0400

    Added configuration for devcontainer development

---
+++ src/main/java/com/timoteoponce/App.java
@@ -0,0 +1,12 @@
+package com.timoteoponce;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        var str = "Hello world!!";
+        System.out.println(str);
+    }
+}
---
+++ src/test/java/com/timoteoponce/AppTest.java
@@ -0,0 +1,18 @@
+package com.timoteoponce;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertEquals(true, Boolean.parseBoolean("true"));
+    }
+}
