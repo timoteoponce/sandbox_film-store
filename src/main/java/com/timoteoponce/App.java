39230a4ba5cf993f8ec807ba2cc624c462ed58d9
Timoteo Ponce <ponce.timoteo@gmail.com>
2022-07-23 15:24:24 +0000

    Fixes minor issues on formatting and version-ref

--- src/main/java/com/timoteoponce/App.java
+++ src/main/java/com/timoteoponce/App.java
@@ -7,10 +7,10 @@ import io.javalin.Javalin;
  *
  */
 public class App {
-    
+
         public static void main(String[] args) {
-             Javalin app = Javalin.create().start(7070);
-            app.get("/", ctx -> ctx.result("Hello World"));
+                Javalin app = Javalin.create().start(7070);
+                app.get("/", ctx -> ctx.result("Hello World"));
         }
-    
+
 }
